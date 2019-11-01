b4542c9d6d3b8e9a8f155960b8ac30b00b4aed5a
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-11-01 03:58:45 -0400

    Remove unused capitalized folder names

--- lua/weapons/PAC_CROWBAR/cl_init.lua
+++
@@ -1,6 +0,0 @@
-include('shared.lua')
-
-SWEP.PrintName	        = "PAC_CROWBAR"
-SWEP.Slot				= 3
-SWEP.SlotPos			= 1
-SWEP.HoldType           = "melee"
--- lua/weapons/PAC_DUAL/init.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile('cl_init.lua')
-AddCSLuaFile('shared.lua')
-
-include('shared.lua')
-SWEP.HoldType = "duel"
-function SWEP:Initialize() 
-	self.Weapon:SetHoldType("duel")
-end 
