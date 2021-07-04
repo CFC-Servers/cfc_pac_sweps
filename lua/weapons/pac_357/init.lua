6ebd3c89b7edbec34d8c721d038a37b199f5d646
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-07-04 23:02:46 +0200

    Fix 357

--- lua/weapons/pac_357/init.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile( "cl_init.lua" )
-AddCSLuaFile( "shared.lua" )
-
-include( "shared.lua" )
-SWEP.HoldType = "revolver"
-function SWEP:Initialize()
-    self:SetHoldType( "revolver" )
-end
