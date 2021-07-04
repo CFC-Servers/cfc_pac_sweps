079c16a9a68b472614ddfd97f118721781fbfa3f
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-07-04 23:02:46 +0200

    Fix 357

--- lua/weapons/pac_357/shared.lua
+++ lua/weapons/pac_357/shared.lua
@@ -1,11 +1,38 @@
+AddCSLuaFile()
 
-AddCSLuaFile( "shared.lua" )
-SWEP.HoldType = "revolver"
+-- General info
+SWEP.Author = "CFC"
+SWEP.Purpose = "Sweps for buildmode PAC."
+SWEP.PrintName = ".357 Magnum"
+SWEP.Category = "PAC SWEPS"
 
-SWEP.DrawAmmo            = false
-SWEP.DrawCrosshair        = false
-SWEP.ViewModelFOV        = 76
-SWEP.ViewModelFlip        = false
+-- Visuals
+SWEP.ViewModel = "models/weapons/c_357.mdl"
+SWEP.WorldModel = "models/weapons/w_357.mdl"
+SWEP.UseHands = true
+SWEP.SetHoldType = "pistol"
+SWEP.DrawCrosshair = true
+
+-- Functionals
+SWEP.Slot = 1
+SWEP.SlotPos = 1
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.AdminOnly = false
+
+-- Ammo and such
+-- Primary
+SWEP.Primary.ClipSize = 1
+SWEP.Primary.DefaultClip = 1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = ""
+
+-- Secondary
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = true
+SWEP.Secondary.Ammo = ""
+
 
 SWEP.Slot                = 3
 SWEP.SlotPos            = 1
@@ -13,41 +40,6 @@ SWEP.IconLetter            = "i"
 SWEP.DrawWeaponInfoBox  = true
 SWEP.Category           = "PAC SWEPS"
 
-------------
--- Info --
-------------
-SWEP.PrintName        = "PAC_357"
-SWEP.Purpose        = "Imitate a 357"
-
-
--------------
--- Misc. --
--------------
-SWEP.Spawnable            = true
-SWEP.AdminSpawnable        = true
-
-
-----------------------
--- Primary Fire --
-----------------------
-SWEP.Primary.NumShots        = 0
-SWEP.Primary.ClipSize        = -1
-SWEP.Primary.DefaultClip    = -1
-SWEP.Primary.Ammo            = "none"
-SWEP.FiresUnderwater         = false
-
-
--------------------------
--- Secondary Fire --
--------------------------
-SWEP.Secondary.ClipSize        = -1
-SWEP.Secondary.DefaultClip    = -1
-SWEP.Secondary.Ammo            = "none"
-
--------------------------
--- Effects/Visual --
--------------------------
-SWEP.ViewModel                = "models/weapons/v_357.mdl";
-SWEP.WorldModel                = "models/weapons/w_357.mdl";
-
-
+function SWEP:PrimaryAttack()
+    self:ShootBullet( 0, 0, 0 )
+end
